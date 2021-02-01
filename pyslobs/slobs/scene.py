5c1e809b918a1cf8ac30babfde776e72a9e556a5
Julian-O <kraken@somethinkodd.com>
2021-02-02 00:50:43 +1100

    Implement rest of Scene methods.

--- pyslobs/slobs/scene.py
+++ pyslobs/slobs/scene.py
@@ -7,6 +7,7 @@ from .factories import (
     scenenode_factory,
     sceneitem_factory,
     sceneitemfolder_factory,
+    selection_factory,
     source_factory,
     register,
 )
@@ -47,13 +48,11 @@ class Scene(SlobsClass):
     def id(self):
         return self._id
 
-    async def add_file(
-        self, path, folder_id: Optional[str] = None
-    ) -> NotImplementedError:
+    async def add_file(self, path, folder_id: Optional[str] = None) -> SceneNode:
         response = await self._connection.command(
             "addFile", self._prepared_params([str(path), folder_id])
         )
-        raise NotImplementedError()
+        raise scenenode_factory(self._connection, response)
 
     async def add_source(
         self, source_id: str, options: Optional[ISceneNodeAddOptions]
@@ -86,46 +85,118 @@ class Scene(SlobsClass):
         response = await self._connection.command("clear", self._prepared_params())
         self._check_empty(response)
 
-    # async def createAndAddSource
-    # async def createFolder
-    # async def getFolder
-    # async def getFolders
-    # async def getItem
-    # async def getItems
+    async def create_and_add_source(
+        self, name: string, type_: TSourceType, settings: Optional[dict[Any]]
+    ) -> SceneItem:
+        response = await self._connection.command(
+            "createAndAddSource", self._prepared_params([name, type_, settings])
+        )
+        return sceneitem_factory(self._connection, response)
+
+    async def create_folder(
+        self, name: string, type_: TSourceType, settings: Optional[dict[Any]]
+    ) -> SceneItem:
+        response = await self._connection.command(
+            "createFolder", self._prepared_params([name, type_, settings])
+        )
+        return sceneitemfolder_factory(self._connection, response)
+
+    async def get_folder(self, scene_folder_id: str):  # -> SceneItemFolder:
+        response = await self._connection.command(
+            "getFolder", self._prepared_params([scene_folder_id])
+        )
+        return sceneitemfolder_factory(self._connection, response)
+
+    async def get_folders(self):  # -> list[SceneItemFolder]:
+        response = await self._connection.command("getFolders", self._prepared_params())
+        return [
+            sceneitemfolder_factory(self._connection, subitem) for subitem in response
+        ]
+
+    async def get_item(self, scene_folder_id: str):  # -> SceneItem:
+        response = await self._connection.command(
+            "getItem", self._prepared_params([scene_folder_id])
+        )
+        return sceneitem_factory(self._connection, response)
+
+    async def get_items(self):  # -> list[SceneItem]:
+        response = await self._connection.command("getItems", self._prepared_params())
+        return [sceneitem_factory(self._connection, subitem) for subitem in response]
 
     async def get_model(self):
         response = await self._connection.command("getModel", self._prepared_params())
         return scene_factory(self._connection, response)
 
-    # async def getNestedItems
-    # async def getNestedScenes
+    async def get_nested_items(self):  # -> Source
+        response = await self._connection.command(
+            "getNestedItems", self._prepared_params()
+        )
+        return [sceneitem_factory(self._connection, source) for source in response]
 
-    # async def getNestedSources
+    async def get_nested_scenes(self):  # -> Source
+        response = await self._connection.command(
+            "getNestedScenes", self._prepared_params()
+        )
+        return [scene_factory(self._connection, source) for source in response]
+
     async def get_nested_sources(self):  # -> Source
         response = await self._connection.command(
             "getNestedSources", self._prepared_params()
         )
         return [source_factory(self._connection, source) for source in response]
 
-    # async def getNode
-    # async def getNodeByName
-    # async def getNodes(refresh=True)
+    async def get_node(self, scene_node_id):  # -> Source
+        response = await self._connection.command(
+            "getNode", self._prepared_params([scene_node_id])
+        )
+        return scenenode_factory(self._connection, source)
+
+    async def get_node_by_name(self, name: str):  # -> Source
+        response = await self._connection.command(
+            "getNodeByName", self._prepared_params([name])
+        )
+        return scenenode_factory(self._connection, source)
+
+    async def get_nodes(self):  # -> Source
+        response = await self._connection.command("getNodes", self._prepared_params())
+        result = [scenenode_factory(self._connection, source) for source in response]
+        # Update attributes as a side-effect
+        self._nodes = result
+        return result
+
     async def get_root_nodes(self):
         response = await self._connection.command(
             "getRootNodes", self._prepared_params()
         )
         return [scenenode_factory(self._connection, node) for node in response]
 
-    # async def getSelection
-    # async def getSource
+    async def get_selection(self, ids: Optional[list[str]]):
+        response = await self._connection.command(
+            "getSelection", self._prepared_params(ids)
+        )
+        return selection_factory(self._connection, response)
+
+    async def get_source(self):
+        response = await self._connection.command(
+            "getSource", self._prepared_params()
+        )
+        return source_factory(self._connection, response)
 
     async def make_active(self):
         response = await self._connection.command("makeActive", self._prepared_params())
         self._check_empty(response)
 
-    # async def remove
-    # async def removeFolder
-    # async def removeItem
+    async def remove(self):
+        response = await self._connection.command("remove", self._prepared_params())
+        self._check_empty(response)
+
+    async def remove_folder(self, folder_id):
+        response = await self._connection.command("removeFolder", self._prepared_params([folder_id]))
+        self._check_empty(response)
+
+    async def remove_item(self, scene_item_id):
+        response = await self._connection.command("removeItem", self._prepared_params([scene_item_id]))
+        self._check_empty(response)
 
     async def set_name(self, new_name):
         response = await self._connection.command(
