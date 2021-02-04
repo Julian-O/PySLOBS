3a8fe338b31fa54056366c53b18ad048bb2580ee
Julian-O <kraken@somethinkodd.com>
2021-02-04 13:28:51 +1100

    Update version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="PySLOBS",
-    version="0.1.5",
+    version="0.1.6",
     author="Julian-O",
     # author_email="",
     description="Python wrapper to StreamLabs OBS API",
