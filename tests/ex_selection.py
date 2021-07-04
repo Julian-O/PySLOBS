ab1d5196ea312b568f129850a973d1d96eda0e0a
Julian-O <kraken@somethinkodd.com>
2021-07-05 05:22:34 +1000

    Exercise more of SelectionService.

--- tests/ex_selection.py
+++ tests/ex_selection.py
@@ -1,6 +1,6 @@
 import asyncio
 from pathlib import Path
-from pyslobs import SelectionService, ScenesService, SourcesService
+from pyslobs import SelectionService, ScenesService, SourcesService, IVec2, ITransform
 
 # import formatters as pp
 from preservers import TestScene
@@ -55,6 +55,9 @@ async def direct_selection(conn) -> None:
         assert middle.id_ in selected_ids
         assert last.id_ in selected_ids
 
+        # Count them
+        assert (await sts.get_size()) == 5
+
         # Deselect two.
         await sts.deselect([first.id_, last.id_])
         selected_ids = await sts.get_ids()
@@ -100,16 +103,65 @@ async def direct_selection(conn) -> None:
         assert not scene_items
 
 
+async def manipulate_selected(conn) -> None:
+    sts = SelectionService(conn)
+    sos = SourcesService(conn)
+    ss = ScenesService(conn)
+
+    path = Path(__file__).parent / "testpattern.jpg"
+
+    async with TestScene(conn) as scene:
+        await ss.make_scene_active(scene.source_id)
+        new_source = await sos.create_source(
+            "test pattern 2", "image_source", {"file": str(path)}, options=None
+        )
+
+        first = await scene.add_source(source_id=new_source.source_id, options=None)
+        await sts.select_all()
+        print(
+            "Bounding rectangle of original test pattern", await sts.get_bounding_rect()
+        )
+        # Centre it.
+        await sts.center_on_screen()
+        # Flip it
+        await sts.flip_x()
+        await sts.flip_y()
+        # Enlarge it
+        await sts.fit_to_screen()
+
+        # Scale it - no origin
+        # Send right code, but doesn't display for unclear reasons.
+        # Tried treating as factor and as final size.
+        await sts.scale(IVec2(5, 5))
+        await sts.scale(IVec2(1, 1), IVec2(2, 2))
+        await sts.scale_with_offset(IVec2(10, 10), IVec2(2, 2))
+        # Enlarge it
+        await sts.stretch_to_screen()
+        await sts.reset_transform()
+        await sts.rotate(45)
+        await sts.set_transform(dict(rotation=12))
+        await sts.set_visibility(False)
+        await sts.set_visibility(True)
+        await sts.set_stream_visible(False)
+        await sts.set_stream_visible(True)
+        await sts.set_recording_visible(False)
+        await sts.set_settings(dict(locked=True))
+        await sts.set_settings(dict(locked=False, recordingVisible=True))
+
+
 async def exercise_selection_ro(conn):
     await display_status(conn)
 
 
 async def exercise_selection_rw(conn):
     await direct_selection(conn)
+    await manipulate_selected(conn)
 
 
 if __name__ == "__main__":
+    import logging
+
+    logging.basicConfig(level=logging.DEBUG)
     from tests.runexercise import run_exercise
 
-    run_exercise(exercise_selection_ro)
     run_exercise(exercise_selection_rw)
