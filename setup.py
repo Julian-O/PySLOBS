84ab07756624ad18e1ee3bbd23e8cd7a476e8b5c
Julian-O <git@somethinkodd.com>
2022-08-02 13:22:24 +1000

    Version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="PySLOBS",
-    version="1.2.4",
+    version="1.2.5",
     author="Julian-O",
     # author_email="",
     description="Python wrapper to StreamLabs Desktop API",
